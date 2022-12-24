179c84f1b4ada7820ec168c1323016535d016103
Mohamed El Mesery <107087703+melmesery@users.noreply.github.com>
2022-12-24 06:06:28 +0200

    Update Basketball.jsx

--- src/Pages/Basketball.jsx
+++ src/Pages/Basketball.jsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "../Styles/Basketball.css";
+import Pages from "../UI/Pages";
 
 const Basketball = () => {
-  
-  return <></>;
+  const [pages, setPages] = useState([]);
+
+  useEffect(() => {
+    fetch("https://my-json-server.typicode.com/melmesery/Sports-Pages/posts")
+      .then((response) => response.json())
+      .then((json) => setPages(json));
+  }, []);
+
+  return (
+    <div className="basketball">
+      <Container>
+        <Row>
+          {pages.map((page) => {
+            return (
+              <Col md={{ span: 4 }} className="d-flex justify-content-center mb-5">
+                <Pages page={page} />
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Basketball;
+
